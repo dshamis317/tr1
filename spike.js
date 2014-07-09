92494bea50a9f62968b8808ced310bde18cb369f
dshamis317 <dshamis317@gmail.com>
2014-07-08 20:48:49 -0400

    created spike file

---
+++ spike.js
@@ -0,0 +1,40 @@
+Gmrr3JDTogvr8i4c32jONzJ8UAtYsDnL
+
+Biotechnology
+Alexion Pharm - ALXN.OQ
+Amgen - AMGN.OQ
+Biogen Idec - BIIB.OQ
+Celgene - CELG.OQ
+Gilead Sciences - GILD.OQ
+Regeneron Pharm - REGN.OQ
+Vertex Pharma - VRTX.OQ
+
+Pharmaceuticuals
+AbbVie - ABBV.N
+Actavis - ACT.N
+Allergan - AGN.N
+Bristol-Myers - BMY.N
+Eli Lilly - LLY.N
+Forest Labs - FRX.N
+Hospira - HSP.N
+Johnson & Johnson - JNJ.N
+Merck & Co. - MRK.N
+Perrigo Company - PRGO.N
+Pfizer - PFE.N
+Zoetis - ZTS.N
+
+Integrated Oil & Gas
+Chevron - CVX.N
+Exxon Mobil - XOM.N
+Hess - HES.N
+Occidental - OXY.N
+
+
+Financial Information
+var url = "https://api.apigarden.thomsonreuters.com/v1/financial/Instruments/RIC:"+ricID+"/pricing?X-TR-API-APP-ID=Gmrr3JDTogvr8i4c32jONzJ8UAtYsDnL"
+
+Headlines
+var url = "https://api.apigarden.thomsonreuters.com/v1/financial/Companies/RIC:"+ricID+"/headlines?X-TR-API-APP-ID=Gmrr3JDTogvr8i4c32jONzJ8UAtYsDnL"
+
+Basic Data
+var url = "https://api.apigarden.thomsonreuters.com/v1/financial/Companies/RIC:"+ricID+"?X-TR-API-APP-ID=Gmrr3JDTogvr8i4c32jONzJ8UAtYsDnL"
